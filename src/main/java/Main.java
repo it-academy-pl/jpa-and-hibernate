12de35d57cba12accb644f066d6b1c4dca49db2e
Rafal Bednarz <raf.bednarz@gmail.com>
2019-05-21 23:29:53 +0200

    Hibernate's session factory added

---
+++ src/main/java/Main.java
@@ -0,0 +1,73 @@
+import dao.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.Date;
+import java.util.Properties;
+
+public class Main {
+
+    private static SessionFactory sessionFactory = null;
+
+    public static void main(String[] args) {
+
+        //Hibernate
+        SessionFactory sessionFactory = getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        //https://docs.jboss.org/hibernate/orm/3.5/javadocs/org/hibernate/Session.html
+        session.close();
+
+
+        //JPA
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("JPAExamples");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction txx = em.getTransaction();
+        tx.begin();
+
+        tx.commit();
+        em.close();
+    }
+
+    private static void createNewStudent(EntityManager em) {
+        Student student = new Student("jpa", "student", "jpa@text.com", "password", new Date());
+        em.persist(student);
+
+    }
+
+    public static SessionFactory getSessionFactory()
+    {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://remotemysql.com:3306/l9CKVZoljX");
+                settings.put(Environment.USER, "l9CKVZoljX");
+                settings.put(Environment.PASS, "r9B2AXe45z");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(Student.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+}
